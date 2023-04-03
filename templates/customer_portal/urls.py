db3193a5c9c70ad19c73e60bb6483d98475b02c2
cliu81530 <126965542+cliu81530@users.noreply.github.com>
2023-04-03 14:15:52 +0200

    Add files via upload

---
+++ templates/customer_portal/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from customer_portal.views import *
+from django.conf.urls import url,re_path
+urlpatterns = [
+    re_path(r'^index/$',index),
+    re_path(r'^login/$',login),
+    re_path(r'^auth/$',auth_view),
+    re_path(r'^logout/$',logout_view),
+    re_path(r'^register/$',register),
+    re_path(r'^registration/$',registration),
+]
