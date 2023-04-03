1545b2e4cb7851696ce78e344a6d8e41c8350040
unknown <mrsalveen@gmail.com>
2023-04-03 14:52:46 +0200

    Adding reservation section

--- bike_app/urls.py
+++ bike_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import Reservation_Page
 
 app_name = 'bike_app'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('reserve/', Reservation_Page.as_view(), name='reserve'),
 ]
--- bike_app/views.py
+++ bike_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from .models import Reservation
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -9,3 +12,9 @@ def home(request):
 def Welcome_page(request):
     #find it on templates DIR of app DIR
     return render(request, "welcome.html")
+
+class Reservation_Page(CreateView):
+    model = Reservation
+    fields = '__all__'
+    success_url = reverse_lazy('home')
+    template_name = 'bike_app/reservation.html'
