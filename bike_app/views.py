ef54502d3ba6045eee0fa14b51335b4db674b423
unknown <mrsalveen@gmail.com>
2023-06-11 22:30:49 +0200

    Adding reservation

--- bike_app/views.py
+++ bike_app/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView 
+from django.views.generic.detail import DetailView
 from .models import Reservation
 from django.urls import reverse_lazy
 
@@ -49,3 +51,30 @@ def bikeList_page(request):
 
 #def bike_list(request):
 #    return render(request, 'bike_list.html')
+
+
+class ReservationCreate(CreateView):
+    model = Reservation
+    fields = '__all__'
+    success_url = reverse_lazy('bike_app:reservations')
+    #template_name = 'bike_app/reservation.html'
+
+class ReservationPage(DetailView):
+    model = Reservation
+    context_object_name = 'reservation'
+
+class ReservationList(ListView): 
+    model = Reservation
+    context_object_name = 'reservations'
+
+class ReservationUpdate(UpdateView):
+    model = Reservation
+    fields = '__all__'
+    success_url = reverse_lazy('bike_app:reservations')
+    #template_name = 'edit_reservation.hthml'
+
+class ReservationDelete(DeleteView):
+    model=Reservation
+    context_object_name='reservation'
+    success_url=reverse_lazy('bike_app:reservations')
+
